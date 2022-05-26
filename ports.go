0fe5cec8181faea1e18309b23773678d2b426d07
Sam Shelton <sam.shelton@soloworks.co.uk>
2022-05-26 10:27:32 +0100

    feat: Cleaner data recieving

--- ports.go
+++ ports.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"log"
 
 	"go.bug.st/serial"
 )
@@ -19,15 +21,28 @@ type device struct {
 }
 
 func (d *device) listen() error {
-	buff := make([]byte, 100)
-	for {
-		n, err := d.dev.Read(buff)
-		if err != nil {
-			return (err)
-		}
-		if n == 0 {
-			return fmt.Errorf("EOF")
-		}
-		fmt.Printf("%v", string(buff[:n]))
+	// buff := make([]byte, 100)
+	// for {
+	// 	n, err := d.dev.Read(buff)
+	// 	if err != nil {
+	// 		return (err)
+	// 	}
+	// 	if n == 0 {
+	// 		return fmt.Errorf("EOF")
+	// 	}
+	// 	fmt.Printf("%v", string(buff[:n]))
+	// }
+
+	scanner := bufio.NewScanner(d.dev)
+	for scanner.Scan() {
+		fmt.Println(scanner.Text()) // Println will add back the final '\n'
 	}
+
+	return scanner.Err()
+}
+
+func (d *device) write(cmd string) error {
+	_, err := d.dev.Write([]byte(fmt.Sprintf("%s\n\r", cmd)))
+	log.Println(fmt.Sprintf("%s\n\r", cmd))
+	return err
 }
